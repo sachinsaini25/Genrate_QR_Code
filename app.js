1bd26cd7dd9d79427f627ab0dc9170f50adcbe15
Sachin Saini <151339859+sachinsaini25@users.noreply.github.com>
2025-08-11 21:41:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const url="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example";
+
+let imgBox=document.getElementById("imgBox");
+let qrImage=document.getElementById("qrImage");
+let qrtext=document.getElementById("qrtext");
+
+function genrateQR(){
+    if(qrtext.value.length > 0){
+    qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+    imgBox.classList.add("show-img");
+    }else{
+        qrtext.classList.add('error');
+        setTimeout(()=>{
+          qrtext.classList.remove('error');            
+        },1000);
+    }
+}
